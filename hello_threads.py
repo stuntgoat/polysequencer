06fc92dfde189b73db68855ec5df3acd672c3921
Isaac Gonzalez <hyperneato@social.local>
2012-02-14 13:07:52 -0500

    completed timing function with Estevo, in the file list_sequence. Adding sequence_generator, which will generate sequences from parent/child sequence relationships

--- hello_threads.py
+++ hello_threads.py
@@ -6,9 +6,7 @@ class HelloThreads(threading.Thread):
     def run(self):
         print("%s says now is %s" % (self.getName(), datetime.datetime.now()))
 
-
 def fire():
-    
     for thread in xrange(10):
         HelloThreads().start()
     return None
