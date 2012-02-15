0177e5923614e5d876fa013f4ed3c173a383651a
Isaac Gonzalez <hyperneato@social.local>
2012-02-14 19:03:19 -0500

    batch_parser.py is able to generate sequences lists for objects that have intervalic relationships between a parent

---
+++ batch_parser.py
@@ -0,0 +1,69 @@
+from sequence_generator import Sequence
+import json
+from json_examples import test0
+
+# de-serialize json_data
+sequence_list = json.loads(test0.json_data)
+
+class BatchSequenceParser(object):
+    """Accepts a list of dicts and return a list of Sequence objects that 
+    are have calculated their sequence intervals based on their relationships
+    with eachother."""
+    
+    def __init__(self, object_list):
+        self.object_list = object_list
+        self.sequence_list = []
+        self.processed_sequence_list = []
+
+    def create_sequence_objects(self):
+        for item in self.object_list:
+            seq = Sequence(parent_name=item['parent'], 
+                           relation_dict=item['relation'],
+                           bpm=item['bpm'],
+                           duration=item['duration'],
+                           file_name=item['audio_filename'],
+                           alias=item['alias'])
+            self.sequence_list.append(seq)
+        return self.sequence_list
+
+    def determine_relationships(self):
+        # acquire root parent sequence
+        completed_sequence_list = []        
+
+        def index_of_root_parent():
+            for seq in self.sequence_list:
+                if seq.parent_name == None: # root parent
+                    seq.process(None) # determines interval and duration from passed variables
+                    return self.sequence_list.index(seq)
+                
+        def parse_child(child):
+            for item in completed_sequence_list:
+                if child.parent_name == item.alias:
+                    child.parent_object = item
+                    child.process(item)
+                    return child
+            return None
+            
+        def process_children():
+            counter = 0
+            while len(self.sequence_list) != 0:
+                for item in self.sequence_list:
+                    next_child = self.sequence_list.pop(0)
+                    processed_child = parse_child(next_child)
+                    if processed_child == None:
+                        self.sequence_list.append(next_child)
+                    else:
+                        completed_sequence_list.append(processed_child)
+            return completed_sequence_list
+        root_parent_index = index_of_root_parent()
+        completed_sequence_list.append(self.sequence_list.pop(root_parent_index))
+        process_children()
+        self.processed_sequence_list = completed_sequence_list
+        return None
+
+if __name__ == "__main__":
+    b = BatchSequenceParser(sequence_list)
+    b.create_sequence_objects()
+    b.determine_relationships()
+    for seq in b.processed_sequence_list:
+        print(seq.timing_list)
--- sequence_generator.py
+++ sequence_generator.py
@@ -8,40 +8,52 @@ class Sequence(object):
     be > 0; relation_dict must == None
     """
     
-    def __init__(self, parent, relation_dict, bpm, duration, file_name):
+    def __init__(self, parent_name=None, relation_dict={}, bpm=0, duration=0, file_name='', alias=""):
         """duration is the total times the sequence will pulse"""
-        self.parent = parent
+        self.parent_name = parent_name
         self.bpm = bpm
         self.relation = relation_dict
         self.duration = duration 
         self.interval = 0 # calculate interval between each pulse
         self.timing_list = [0]
         self.filename = file_name
+        self.parent_object = None
+        self.alias = alias
+    
 
-    def calculate_duration(self):
+    def calculate_interval(self):
         """ Root parents caclulate duration from self.bpm; 
         children calculate duration based on relationship to parent"""
-        if parent == None:
-            self.interval = 60.0/bpm
+        if self.parent_object == None:
+            self.interval = 60.0/self.bpm
             return self.interval
         else:
             # duration is (self.relation["parent"] * self.parent.duration) / self.relation["self"]
-            self.interval = ((self.relation["parent"] * self.parent.interval) / self.relation["self"])
+            self.interval = ((self.relation["parent"] * self.parent_object.interval) / self.relation["self"])
             return self.interval
 
     def calculate_duration(self):
         """If duration == None, play until the end of the parents sequence; otherwise, play for
         duration pulses"""
-        if duration == None: # instance has a parent
-            parent_total_time = self.parent.interval * self.parent.duration
+        if self.parent_object != None: # instance has a parent
+            parent_total_time = self.parent_object.interval * self.parent_object.duration
             self.duration = int(floor(parent_total_time / self.interval))
-        else:
+        else: # instance is a root parent
             return self.duration
 
     def calculate_timing_list(self):
         """Calculate timing as list of incrementing sum of durations"""
         duration_sum = 0
-        for pulse in xrange(duration):
+        for pulse in xrange(self.duration):
             duration_sum += self.interval
             self.timing_list.append(duration_sum)
         return self.timing_list
+    
+    def process(self, parent_object):
+        self.parent_object = parent_object
+        self.calculate_interval()
+        self.calculate_duration()
+        self.calculate_timing_list()
+        return None
+            
+        
