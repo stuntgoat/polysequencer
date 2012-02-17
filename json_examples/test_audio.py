9267a147610fea8c06a27a12613bc65952c4653c
Isaac <hyperneato@gmail.com>
2012-02-17 15:10:53 -0500

    added test audio

--- json_examples/test_audio.py
+++ json_examples/test_audio.py
@@ -1,5 +1,5 @@
 """Install gstreamer, gst-python(needed pykg-config, in place of missing pkg-config)"""
-
+from os import fork
 from pygame import mixer as m
 from time import sleep
 from threading import Thread
@@ -7,35 +7,40 @@ from threading import Thread
 
 sound_list = []
 m.init()
-sound_list.append(m.Sound('hey.ogg'))
-sound_list.append(m.Sound('hi.ogg'))
-sound_list.append(m.Sound('ho.ogg'))
-
-
-class ThreadedSoundPlayer(Thread):
-    def __init__(self, channel, sound):
-        super(ThreadedSoundPlayer, self).__init__()
-        self.channel = channel
-        self.sound = sound
-    def run(self):
-        self.channel.play(self.sound)
-        sleep(2)
+sound_list.extend([m.Sound('hey.ogg'), m.Sound('hi.ogg'), m.Sound('ho.ogg')])
 
 channel_list = []
 for i in xrange(8):
     channel_list.append(m.Channel(i))
-    
-def play_sounds():
+
+def play_sounds_with_fork():
     for channel in channel_list:
-        tsp = ThreadedSoundPlayer(channel, sound_list[channel_list.index(channel)%3])
-        tsp.start()
-
+        pid = fork()
+        print pid
+        channel.play(sound_list[channel_list.index(channel)%3])
+        sleep(.2)
+        
 if __name__ == "__main__":
-
-    play_sounds()
+    play_sounds_with_fork()
+    # play_sounds()
     sleep(4)
 
 
     
 
 
+
+# class ThreadedSoundPlayer(Thread):
+#     def __init__(self, channel, sound):
+#         super(ThreadedSoundPlayer, self).__init__()
+#         self.channel = channel
+#         self.sound = sound
+#     def run(self):
+#         pid = fork()
+#         if pid != 0:
+#             self.channel.play(self.sound)
+        
+# def play_sounds():
+#     for channel in channel_list:
+#         tsp = ThreadedSoundPlayer(channel, sound_list[channel_list.index(channel)%3])
+#         tsp.start()
